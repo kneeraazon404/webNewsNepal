7c242276a0c8249d41cfabae51d3dc32c106e0b6
kneeraazon01 <karkinirajan1999@gmail.com>
2021-03-17 01:21:04 +0545

    heroku in the settings added

--- newssite/settings.py
+++ newssite/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -150,3 +151,4 @@ LOGIN_URL = "login"
 from django.contrib.messages import constants as messages
 
 MESSAGE_TAGS = {messages.ERROR: "danger"}
+django_heroku.settings(locals())
